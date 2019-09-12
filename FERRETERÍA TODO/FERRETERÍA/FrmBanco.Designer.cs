22dcdd5f110558a264c6ffc2ac9ccdd75d0d4d56
Gustavo <busigusi1@gmail.com>
2019-09-11 21:42:25 -0500

    cambios ejemplo

--- FERRETERÍA TODO/FERRETERÍA/FrmBanco.Designer.cs
+++ FERRETERÍA TODO/FERRETERÍA/FrmBanco.Designer.cs
@@ -116,9 +116,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(267, 59);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(204, 13);
+            this.label3.Size = new System.Drawing.Size(140, 13);
             this.label3.TabIndex = 8;
-            this.label3.Text = "Inserta Descripción ejemplo BANCOMER)";
+            this.label3.Text = "Inserta Descripción ejemplo ";
             // 
             // GrvBanco
             // 
